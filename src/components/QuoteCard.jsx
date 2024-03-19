f42a7afd930a99661d65391da835d925e7748385
Tapas Adhikary <tapas.adhikary@gmail.com>
2024-03-19 22:21:48 +0530

    A few CSS changes

--- src/components/QuoteCard.jsx
+++ src/components/QuoteCard.jsx
@@ -1,8 +1,8 @@
 
 const QuoteCard = ({quote}) => {
   return (
-    <div className="m-4 border border-gray-200 p-1 rounded-sm flex flex-col w-64 h-36 bg-gray-50 transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300 ">
-      <p className="text-xl text-center m-auto">{quote.text}</p>
+    <div className="card">
+      <p className="text-xl text-center m-auto leading-normal">{quote.text}</p>
     </div>
   )
 }
